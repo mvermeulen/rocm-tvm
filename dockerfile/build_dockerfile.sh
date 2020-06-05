02d12f49b5cd75ab8107302c7045fad3b7270b70
Mike Vermeulen <mike@vermeulen.com>
2020-06-05 09:58:49 -0500

    Update to add libssl-dev.

--- dockerfile/build_dockerfile.sh
+++ dockerfile/build_dockerfile.sh
@@ -25,7 +25,7 @@ echo "RUN rm /etc/apt/sources.list.d/rocm.list" >> ${DOCKERFILE}
 # Install prerequisite packages (llvm build, ROCm runtime, TVM build)
 echo "ENV PATH=/usr/local/llvm/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" >> ${DOCKERFILE}
 echo "ENV DEBIAN_FRONTEND=noninteractive" >> ${DOCKERFILE}
-echo "RUN apt update && apt install -y git wget libz3-dev libxml2-dev openssl" >> ${DOCKERFILE}
+echo "RUN apt update && apt install -y git wget libz3-dev libxml2-dev openssl libssl-dev" >> ${DOCKERFILE}
 echo "RUN cd /src && wget https://github.com/Kitware/CMake/releases/download/v3.17.3/cmake-3.17.3.tar.gz && tar xf cmake-3.17.3.tar.gz && cd cmake-3.17.3 && ./configure && make && make install" >> ${DOCKERFILE}
 echo "RUN apt update && apt install -y rocm-libs miopen-hip" >> ${DOCKERFILE}
 echo "RUN apt update && apt install -y python python-dev python-setuptools gcc libtinfo-dev zlib1g-dev build-essential python3 python3-pip" >> ${DOCKERFILE}
