abe91282a600c592935ef1a3cc8ca3d9bad4de3d
Mike Vermeulen <mike@vermeulen.com>
2020-11-09 13:20:47 -0600

    Update list of unit tests.

--- qa/run_rocm_unittest.sh
+++ qa/run_rocm_unittest.sh
@@ -10,11 +10,12 @@ ROCM_TVM_HOME=${ROCM_TVM_HOME:="/src/rocm-tvm"}
 
 cd ${TVM_HOME}/tests/python/unittest
 # no output on success
+python3 test_target_codegen_bool.py
 python3 test_target_codegen_rocm.py
 python3 test_target_codegen_device.py
-python3 test_target_codegen_bool.py
 python3 test_target_target.py
 # expected output: "Testing using contexts: [cpu(0),rocm(0)]
 python3 test_runtime_ndarray.py
 # expected output: "Running on target: rocm", along with no errors
 python3 test_te_tensor_overload.py
+python3 test_tir_transform_lower_warp_memory.py
