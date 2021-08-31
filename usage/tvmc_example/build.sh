bb443b58c4cd4a237ddd780c611f424c2ab39796
Mike Vermeulen <mike@vermeulen.com>
2021-08-31 16:39:15 -0500

    Add tuning.

--- usage/tvmc_example/build.sh
+++ usage/tvmc_example/build.sh
@@ -18,6 +18,18 @@ python3 preprocess.py
 # Running the compiled model
 # What is needed to run a rocm target?
 tvmc -v run --inputs imagenet_cat.npz --output predictions.npz resnet50i1.tar
-
-# Postprocess
 python3 postprocess.py
+
+# Tune the model
+tvmc tune --target "llvm" --output resnet50i1-autotuner.json ${SAVED_MODELDIR}/torchvision/resnet50i1.onnx
+
+# Compile with tuned data
+tvmc compile --target "llvm" --tuning-records resnet50i1-autotuner.json --output resnet50i1-tuned.tar ${SAVED_MODELDIR}/torchvision/resnet50i1.onnx
+
+# Run the compiled model
+tvmc -v run --inputs imagenet_cat.npz --output predictions.npz resnet50i1-tuned.tar
+python3 postprocess.py
+
+# Try tuning comparisons
+tvmc run --inputs imagenet_cat.npz --output predictions.npz --print-time --repeat 1000 resnet50i1.tar 
+tvmc run --inputs imagenet_cat.npz --output predictions.npz --print-time --repeat 1000 resnet50i1-tuned.tar 
