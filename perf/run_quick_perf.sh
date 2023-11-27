421e6bb1f86d8b67968c7e2841798110221b21ea
Mike Vermeulen <mike@vermeulen.com>
2023-11-27 12:27:27 -0700

    Fix up references to target since it can be more than one token.

--- perf/run_quick_perf.sh
+++ perf/run_quick_perf.sh
@@ -31,26 +31,26 @@ do
 	
 	echo "\n--- Model: $model,  Device: $device, Target: $target"
 	
-	echo tvmc compile --target $target -o ${model}.${target}.tar $full_model
-	tvmc compile --target "$target" -o ${model}.${target}.tar $full_model \
+	echo tvmc compile --target $target -o ${model}.${label}.tar $full_model
+	tvmc compile --target "$target" -o ${model}.${label}.tar $full_model \
 	     1> ${model}.${label}.compile.out 2> ${model}.${label}.compile.err
 	
-	echo tvmc run --device $device --fill-mode random --print-time --repeat 100 ${model}.${target}.tar
-	tvmc run --device $device --fill-mode random --print-time --repeat 100 ${model}.${target}.tar \
+	echo tvmc run --device $device --fill-mode random --print-time --repeat 100 ${model}.${label}.tar
+	tvmc run --device $device --fill-mode random --print-time --repeat 100 ${model}.${label}.tar \
 	     1> ${model}.${label}.run.out 2> ${model}.${label}.run.err
 
 	if [ "$RUN_TUNE" = "1" ]; then
 	
-	    echo tvmc tune --target $target --output ${model}.${target}.json $full_model
-	    tvmc tune --target $target --output ${model}.${target}.json $full_model \
+	    echo tvmc tune --target $target --output ${model}.${label}.json $full_model
+	    tvmc tune --target "$target" --output ${model}.${label}.json $full_model \
 		 1> ${model}.${label}.tune.out 2>${model}.${label}.tune.err
 	
-	    echo tvmc compile --target "$target" -o ${model}.${target}.tuned.tar --tuning-records ${model}.${target}.json $full_model
-	    tvmc compile --target $target -o ${model}.${target}.tuned.tar --tuning-records ${model}.${target}.json $full_model \
+	    echo tvmc compile --target "$target" -o ${model}.${label}.tuned.tar --tuning-records ${model}.${label}.json $full_model
+	    tvmc compile --target "$target" -o ${model}.${label}.tuned.tar --tuning-records ${model}.${label}.json $full_model \
 		 1> ${model}.${label}.compilet.out 2> ${model}.${label}.compilet.err
 	
-	    echo tvmc run --device $device --fill-mode random --print-time --repeat 100 ${model}.${target}.tuned.tar
-	    tvmc run --device $device --fill-mode random --print-time --repeat 100 ${model}.${target}.tuned.tar \
+	    echo tvmc run --device $device --fill-mode random --print-time --repeat 100 ${model}.${label}.tuned.tar
+	    tvmc run --device $device --fill-mode random --print-time --repeat 100 ${model}.${label}.tuned.tar \
 		 1> ${model}.${label}.runt.out 2> ${model}.${label}.runt.err
 
 	fi
