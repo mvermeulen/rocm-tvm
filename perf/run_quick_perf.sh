3be089697f15b5d509743588ee0fa13470a694b0
Mike Vermeulen <mike@vermeulen.com>
2023-11-27 11:41:06 -0600

    Update script directories.

--- perf/run_quick_perf.sh
+++ perf/run_quick_perf.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-MODEL=${MODEL:="/home/mev/source/rocm-migraphx/saved-models/torchvision/resnet50i1.onnx"}
-model_name=`basename $MODEL .onnx`
+DATESTAMP=`date '+%Y%m%d-%H%M'`
+mkdir ${DATESTAMP}
+cd ${DATESTAMP}
+RUN_TUNE=${RUN_TUNE:="0"}
 
 models=(
     '/home/mev/source/rocm-migraphx/saved-models/torchvision/resnet50i1.onnx'
     '/home/mev/source/rocm-migraphx/saved-models/torchvision/resnet50i64.onnx'
     '/home/mev/source/rocm-migraphx/saved-models/onnx-model-zoo/inception-v2-9.onnx'
 )
-labels=('cpu' 'rocm' 'hip' 'rocmlib')
-targets=('llvm' 'rocm' 'hip' 'rocm -libs=miopen,rocblas')
-devices=('cpu' 'rocm' 'rocm' 'rocm')
+labels=('cpu' 'rocm' 'hip' 'rocmlib' 'ptx' 'cuda' 'ptxlib' 'cudalib')
+targets=('llvm' 'rocm' 'hip' 'rocm -libs=miopen,rocblas' 'ptx' 'cuda' 'ptx -libs=cudnn,cublas' 'cuda -libs=cudnn,cublas')
+devices=('cpu' 'rocm' 'rocm' 'rocm' 'cuda' 'cuda' 'cuda' 'cuda')
 
 for full_model in ${models[*]}
 do
@@ -20,6 +22,9 @@ do
 	label=${labels[$i]}
 	target=${targets[$i]}
 	device=${devices[$i]}
+
+	mkdir $model
+	cd $model
 	
 	echo "\n--- Model: $model,  Device: $device, Target: $target"
 	
@@ -30,17 +35,24 @@ do
 	echo tvmc run --device $device --fill-mode random --print-time --repeat 100 ${model}.${target}.tar
 	tvmc run --device $device --fill-mode random --print-time --repeat 100 ${model}.${target}.tar \
 	     1> ${model}.${label}.run.out 2> ${model}.${label}.run.err
+
+	if [ "$RUN_TUNE" = "1" ]; then
 	
-	echo tvmc tune --target $target --output ${model}.${target}.json $full_model
-	tvmc tune --target $target --output ${model}.${target}.json $full_model \
-	     1> ${model}.${label}.tune.out 2>${model}.${label}.tune.err
+	    echo tvmc tune --target $target --output ${model}.${target}.json $full_model
+	    tvmc tune --target $target --output ${model}.${target}.json $full_model \
+		 1> ${model}.${label}.tune.out 2>${model}.${label}.tune.err
 	
-	echo tvmc compile --target $target -o ${model}.${target}.tuned.tar --tuning-records ${model}.${target}.json $full_model
-	tvmc compile --target $target -o ${model}.${target}.tuned.tar --tuning-records ${model}.${target}.json $full_model \
-	     1> ${model}.${label}.compilet.out 2> ${model}.${label}.compilet.err
+	    echo tvmc compile --target $target -o ${model}.${target}.tuned.tar --tuning-records ${model}.${target}.json $full_model
+	    tvmc compile --target $target -o ${model}.${target}.tuned.tar --tuning-records ${model}.${target}.json $full_model \
+		 1> ${model}.${label}.compilet.out 2> ${model}.${label}.compilet.err
 	
-	echo tvmc run --device $device --fill-mode random --print-time --repeat 100 ${model}.${target}.tuned.tar
-	tvmc run --device $device --fill-mode random --print-time --repeat 100 ${model}.${target}.tuned.tar \
-	     1> ${model}.${label}.runt.out 2> ${model}.${label}.runt.err
+	    echo tvmc run --device $device --fill-mode random --print-time --repeat 100 ${model}.${target}.tuned.tar
+	    tvmc run --device $device --fill-mode random --print-time --repeat 100 ${model}.${target}.tuned.tar \
+		 1> ${model}.${label}.runt.out 2> ${model}.${label}.runt.err
+
+	fi
+
+	cd ..
+
     done    
 done
